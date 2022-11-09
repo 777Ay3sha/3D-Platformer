d348656ddbefa84144a8025436f7559537a85ea1
777Ay3sha <113114259+777Ay3sha@users.noreply.github.com>
2022-11-09 10:44:33 +0000

    made changes

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+     
+    float maxSpeed = 1.0f;
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    GameObject cam;
+    float rotationSpeed = 2.0f;
+    float camRotationSpeed = 1.5f;
+
+    void Start()
+    {
+       cam = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+
+        rotation = rotation = Input.GetAxis("Mouse X");
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation,0.0f));
+        
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+    }
+}
